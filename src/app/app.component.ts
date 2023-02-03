5747bc2f6981453f947e379c1a137d728575ebec
Jonathan <jonathan_corro@hotmail.com>
2023-02-03 01:33:49 -0500

    Tarea finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
+  
 export class AppComponent {
-  title = 'botones';
+
+ 
+  
+  title = 'Tarea solicitada por Mr. Yosel';
+  
+  
+  aumenta: number[] = [10,20,30,40,50,60,70,80];
+
+  
+
+
+  ResultadoBtn( aumenta: number){
+    
+      console.log("El numero pickado es:" + aumenta);
+    
+  }
+  
 }
